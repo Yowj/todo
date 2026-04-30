4e0e2287c3c7ecbd8d1ace773e5b7f8cf74d6403
Yowj <eugenecoderx@gmail.com>
2026-04-30 09:36:35 +0800

    Add optimistic in add btn

--- frontend/src/hooks/todo/useAddTodo.js
+++ frontend/src/hooks/todo/useAddTodo.js
@@ -10,22 +10,31 @@ const useAddTodo = () => {
       const response = await axiosInstance.post("/todo/add-todo", todoData);
       return response.data;
     },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: ["fetchTodo"] });
-      toast.success(data.message);
+    onMutate: async (todoData) => {
+      await queryClient.cancelQueries({ queryKey: ["fetchTodo"] });
+      const prev = queryClient.getQueryData(["fetchTodo"]);
+      queryClient.setQueryData(["fetchTodo"], (old) => ({
+        ...old,
+        todos: [...old.todos, todoData],
+      }));
+      toast.success("Todo Added");
+      return { prev };
     },
-    onError: (error) => {
-      toast.error("Failed to add task. Please try again.");
-      console.error("Add todo error:", error);
-    }
+    onError: (_err, _todoData, context) => {
+      queryClient.setQueryData(["fetchTodo"], context.prev);
+      toast.error("Failed to add.");
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["fetchTodo"] });
+    },
   });
 
-  return { 
-    addTodoMutate: addTodo.mutate, 
-    data: addTodo.data, 
+  return {
+    addTodoMutate: addTodo.mutate,
+    data: addTodo.data,
     isLoadingTodo: addTodo.isPending,
     isSuccess: addTodo.isSuccess,
-    isError: addTodo.isError
+    isError: addTodo.isError,
   };
 };
 
